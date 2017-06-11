fbec1cbf880349d5182b6677462729032daaf1fd
Alan Seto <alantseto@gmail.com>
2017-06-10 23:13:17 -0500

    Save user passwords with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -18,5 +18,23 @@ const userSchema = new Schema({
     timestamps: true
   }
 );
+
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      console.log(hash);
+      next();
+    });
+  });
+});
+
 
 module.exports = mongoose.model('User', userSchema);
