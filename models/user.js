ddbc09625203b94912bfe89e41c8591c5769d82e
Alan Seto <alantseto@gmail.com>
2017-06-16 12:38:55 -0500

    Minor fixes

--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ userSchema.pre('save', function (next) {
       if (err) { return next(err); }
 
       user.password = hash;
-      console.log(hash);
+      
       next();
     });
   });
--- services/passport.js
+++ services/passport.js
@@ -2,10 +2,10 @@ const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
-
 const User = require('../models/user');
 
 const localOptions = { usernameField: 'email' };
+
 const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
   User.findOne({ email }, (err, user) => {
     if (err) { return done(err); }
